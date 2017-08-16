2e6f2084316b37bfe72a4896c2be8f08fe086c6d
AYUSH SHUKLA <ayush.shuklash@ves.ac.in>
2017-08-17 01:28:12 +0530

    MODEL CREATED

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @content="JAI MATA DI"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Blog::Application.routes.draw do
-  get "home/index"
+  resources :posts
+  # get "home/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
