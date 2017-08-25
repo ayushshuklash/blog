b4fa9df9242bd6f1054730ed67e760c9ca5808d4
AYUSH SHUKLA <ayush.shuklash@ves.ac.in>
2017-08-26 02:03:44 +0530

    Add Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,13 @@
 class CategoriesController < ApplicationController
+  def new
+    @category     = Category.new
+  end
+  def create
+    @category = Category.new(params[:category])
+      if @category.save
+        redirect_to new_category_path, :notice => "AAPKA POST ADD HO GAYA HAIN"
+      else
+        render"new"
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Blog::Application.routes.draw do
   root :to => 'posts#index'
   resources :posts
+  resources :categories ,:except =>[:destroy]
   # get "home/index"
 
   # The priority is based upon order of creation:
